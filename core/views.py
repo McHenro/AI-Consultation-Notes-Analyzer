9c3b5f3abfc0ca423d4b3a778349f13c1908735d
Henry Anorue <hanorue@drohealth.com>
2026-01-08 16:16:06 +0100

    Wire Celery into the API

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import NoteAnalysis
 from .serializers import NoteAnalysisSerializer
+from .tasks import analyze_note_task
 
 
 class NoteAnalysisViewSet(ModelViewSet):
     queryset = NoteAnalysis.objects.all().order_by("-created_at")
     serializer_class = NoteAnalysisSerializer
+
+    def perform_create(self, serializer):
+        analysis = serializer.save()
+        analyze_note_task.delay(analysis.id)
